9ed80c22ea41d1c457015b37ea65ec0f73f21be9
Laetitia Langlois <laetitia.langlois@cosmian.com>
2023-05-24 09:29:26 +0200

    fix: table cells ellipsis

--- src/Views/Edit.tsx
+++ src/Views/Edit.tsx
@@ -8,43 +8,44 @@ import { paths_config } from "../config/paths"
 import { ConfigurationInfo, MetaData, downloadFile } from "../utils/utils"
 import "./style.less"
 
+const ellipsisStyle: React.CSSProperties = { maxWidth: 200, whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis" }
+
 const columns = [
   {
     title: "Column name",
     dataIndex: "name",
     key: "name",
-    width: "10",
   },
   {
     title: "Type",
     dataIndex: "type",
     key: "type",
-    width: "10",
     render: (type: string) => {
       return <Tag>{type}</Tag>
-    }
+    },
   },
   {
     title: "Example",
     dataIndex: "example",
     key: "example",
-    width: "30",
+    render: (example: string) => {
+      return <div style={ellipsisStyle}>{example}</div>
+    },
   },
   {
     title: "Method",
     dataIndex: "method",
     key: "method",
-    width: "20",
   },
   {
     title: "Result",
     dataIndex: "result",
     key: "result",
-    width: "30",
-    render: (result: string | number ) => {
-      if (result && result.toString().substring(0, 5) === "Error") return <div style={{ color: "#e34319", fontStyle: "italic" }}>{result}</div>
-      return <div>{result}</div>
-    }
+    render: (result: string | number) => {
+      if (result && result.toString().substring(0, 5) === "Error")
+        return <div style={{ color: "#e34319", fontStyle: "italic", ...ellipsisStyle }}>{result}</div>
+      return <div style={ellipsisStyle}>{result}</div>
+    },
   },
 ]
 
@@ -58,7 +59,7 @@ const Edit = (): JSX.Element => {
 
   useEffect(() => {
     const fetchConfig = async (): Promise<void> => {
-      const configuration: { configurationInfo: ConfigurationInfo, metadata: MetaData[] } | null = await localForage.getItem(configUuid)
+      const configuration: { configurationInfo: ConfigurationInfo; metadata: MetaData[] } | null = await localForage.getItem(configUuid)
       if (configuration) {
         setConfigurationInfo(configuration.configurationInfo)
         setFileMetadata(configuration.metadata)
@@ -87,7 +88,7 @@ const Edit = (): JSX.Element => {
       notification.error({
         duration: 3,
         message: "Error saving configuration",
-        description: (error as Error).message
+        description: (error as Error).message,
       })
       throw new Error((error as Error).message)
     }
@@ -96,18 +97,13 @@ const Edit = (): JSX.Element => {
   return (
     <div className="edit-view">
       <div className="edit-main">
-        <BackArrow
-          onClick={() => navigate(paths_config.home)}
-          text="Back to configurations list"
-        />
+        <BackArrow onClick={() => navigate(paths_config.home)} text="Back to configurations list" />
         <div className="head">
           <div className="head-titles">
             <h1>{configurationInfo?.name} anonymization columns</h1>
             <div>Select column(s) and define method to apply.</div>
           </div>
-          <Button onClick={() => downloadConfiguration(configurationInfo?.uuid)}>
-            Download configuration
-          </Button>
+          <Button onClick={() => downloadConfiguration(configurationInfo?.uuid)}>Download configuration</Button>
         </div>
         <RoundedFrame>
           <Table
@@ -116,10 +112,17 @@ const Edit = (): JSX.Element => {
             columns={columns}
             pagination={false}
             rowSelection={rowSelection}
+            tableLayout="auto"
+            scroll={{ x: 800 }}
           />
         </RoundedFrame>
       </div>
-      <EditMethodBox selectedRowKeys={selectedRowKeys} fileMetadata={fileMetadata} saveConfiguration={saveConfiguration} setSelectedRowKeys={setSelectedRowKeys} />
+      <EditMethodBox
+        selectedRowKeys={selectedRowKeys}
+        fileMetadata={fileMetadata}
+        saveConfiguration={saveConfiguration}
+        setSelectedRowKeys={setSelectedRowKeys}
+      />
     </div>
   )
 }
